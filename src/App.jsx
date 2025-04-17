26a8b1b3b4b62a9d073572f40a77a8d448a9215b
raven252525 <maximyurkovsky@gmail.com>
2025-04-16 22:17:26 -0700

    updated Mobile menu modal, and filled in home component in sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import { LoadingScreen } from './components/LoadingScreen'
 import { Navbar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           ${isLoaded ? "opacity-100 ": "opacity-0"} bg-black text-gray-100`}>
             <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
             <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <Home />
         </div>
     </>
   )
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -4,7 +4,7 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) => {
     return (
         <div className={`fixed top-0 left-0 w-full bg-gray-900 z-40 flex flex-col items-center
          justify-center transition-all duration-300 ease-in-out
-        ${menuOpen ? "h-screen opacity-50 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
+        ${menuOpen ? "h-screen opacity-89 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
 
 
 
