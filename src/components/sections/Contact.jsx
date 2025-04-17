3471a41e0f83ba647a6b5fe376ff772d324ef291
raven252525 <maximyurkovsky@gmail.com>
2025-04-17 02:33:35 -0700

    hooked up Web API for emailing

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,28 +1,51 @@
+import { useState } from "react"
 import { RevealOnScroll } from "../RevealOnScroll"
+import emailjs from 'emailjs-com'
 
 
 export const Contact = () => {
+    
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY).then((result) => {
+            alert("Message Sent!")
+            setFormData({name: "", email: "", message: ""})
+        }).catch(() => alert("Error, email not sent"))
+    }
+    
+    
+    
     return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
             <div className="px-4 w-150">
 
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r
                 from-blue-500 to-purple-600 text-center bg-clip-text text-transparent "> Get In Touch</h2>
-                <form className="space-y-6">
+                <form className="space-y-6" onSubmit={handleSubmit}>
                     <div className="relative">
-                        <input type="text" id="name" name="name" required 
+                        <input type="text" id="name" name="name" required value={formData.name}
+                        onChange={(e) => setFormData({...formData, name: e.target.value})}
                         className="w-full bg-white/5 border border-white/10 rounded px-4 py-3
                          text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                          placeholder="Name..."/>
                     </div>
                     <div className=" relative">
-                        <input type="email" id="email" name="email" required 
+                        <input type="email" id="email" name="email" required value={formData.email}
+                        onChange={(e) => setFormData({...formData, email: e.target.value})}
                         className="w-full bg-white/5 border border-white/10 rounded px-4 py-3
                          text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                          placeholder="example@gmail.com"/>
                     </div>
                     <div className=" relative">
-                        <textarea id="message" name="message" required rows={5}
+                        <textarea id="message" name="message" required rows={5} value={formData.message}
+                        onChange={(e) => setFormData({...formData, message: e.target.value})}
                         className="w-full bg-white/5 border border-white/10 rounded px-4 py-3
                          text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                          placeholder="Your Message..."/>
