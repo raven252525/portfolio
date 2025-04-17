8ffd7cb27e589b3a3cf3c863f5771601b5959d42
raven252525 <maximyurkovsky@gmail.com>
2025-04-17 13:57:18 -0700

    still working on making education and experience same length borders, afterwards add projects, and link to github

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -11,11 +11,11 @@ export const About = () => {
     
     return <section id="about" className="min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
-            <div className="max-w-3xl mx-auto px-4">
+            <div className="max-w-3xl mx-auto px-4 ">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r
                 from-blue-500 to-purple-600 text-center bg-clip-text text-transparent "> About Me </h2>
             
-                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all mb-6">
                     <p className="text-gray-300 mb-6">
                     a Computer Science student with a passion for building scalable tools,
                      tackling real-world problems with code, and helping others along the way.
@@ -66,8 +66,8 @@ export const About = () => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-rows-2 gap-6 mt-8">
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                <div className="grid grid-cols-1 md:grid-rows-2 gap-6">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all ">
                             <h3 className="text-2xl font-bold mb-4"> Education</h3>
                             <ul className="list-disc list-inside text-gray-300 space-y-2"> 
                                 <li className="font-bold text-xl">
@@ -76,7 +76,7 @@ export const About = () => {
                                 <li className="text-xl">
                                     Relevant Coursework: <span className="text-lg text-gray-400 grid grid-cols-2">
                                         {relevantCoursework.map((course, key) => (
-                                        <span key={key} className="flex flex-col m-3">
+                                        <span key={key} className="flex flex-col m-3  hover:text-blue-500 transition-colors">
                                             {course == relevantCoursework[relevantCoursework.length - 1] 
                                             ? course
                                             : `${course}, `}
@@ -88,23 +88,42 @@ export const About = () => {
                     </div>
 
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                            <div className="flex flex-row">
-                                <h3 className="text-xl font-bold mb-4"> Experience</h3> <h2 className="right-5">-my professional journey</h2>
+                            <div className="flex justify-between">
+                                <h3 className="text-xl font-bold mb-4"> Experience</h3> <h2 className="text-gray-400 hover:text-blue-500 transition"> -my professional journey</h2>
                             </div>
                             <div className="space-y-4 text-gray-300">
                                 <div>
-                                    <h4 className="font-semibold"> Student Research Asisstant at Universty of California, Davis (12/2024 - present)</h4>
-                                    <p className=" text-gray-400 text-sm py-2"> ● Led debugging sessions in C++ and Python, resolving tickets, and issues in CMake, GitHub version control </p>
-                                    <p className=" text-gray-400 text-sm py-"> ● Educated 50+ students one on one with in depth tutorials, increasing understanding and average student engagement by
+                                <div className="flex justify-between">
+                                        <h4 className="font-semibold px-2"> Student Research Assistant </h4>
+                                        <span className="text-md font-light"> University of California, Davis (12/2024 - present)</span>
+                                    </div>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Led debugging sessions in C++ and Python, resolving tickets, and issues in CMake, GitHub version control </p>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Educated 50+ students one on one with in depth tutorials, increasing understanding and average student engagement by
                                     10% via explanation of topics such as dynamic memory allocation, pointer arithmetic, and dictionary/list comprehension</p>
                                 </div>
                                 <div>
-                                    <h4 className="font-semibold"> Analytics Intern II, at Contra Costa County WIC (06/2023 - 07/2024)</h4>
-                                    <p className=" text-gray-400 text-sm py-2"> ● Utilized custom database, Excel, and Microsoft Office to process client and employee data entry, review, and extraction,
-                                    optimizing and thus reducing 20 hours of employee work weekly on averag </p>
-                                    <p className=" text-gray-400 text-sm py-2">  </p>
-                                    <p className=" text-gray-400 text-sm py-2">  </p>
+                                    <div className="flex justify-between">
+                                        <h4 className="font-semibold px-2"> Analytics Intern II </h4>
+                                        <span className="text-md font-light"> Contra Costa County WIC (06/2023 - 07/2024)</span>
+                                    </div>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Utilized custom database, Excel, and Microsoft Office to process client and employee data entry, review, and extraction,
+                                    optimizing and thus reducing 20 hours of employee work weekly on average </p>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Provided 300+ client information sessions for 50 participants, providing nutritional content, troubleshooting program </p>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Supported over 10 custom and commercial client projects in 5 week sprints, ensuring seamless program operations </p>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Streamlined clerical operations by incorporating a systematic data review protocol; identified and corrected over 200
+                                    inaccurate entries within a month, resulting in a notable enhancement in overall data accuracy</p>
                                 </div>
+
+                                <div>
+                                    <div className="flex justify-between">
+                                        <h4 className="font-semibold px-2"> Chemistry Tutor </h4>
+                                        <span className="text-md font-light"> Diablo Valley College (08/2023 - 12/2023)</span>
+                                    </div>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Facilitated over 100 individual and group tutoring sessions in chemistry, facilitating problem-solving for 150 students </p>
+                                    <p className=" text-gray-400 text-sm py-2 hover:text-blue-500 transition-colors"> ● Employed the Socratic Method, and worked with over 5 Professors to help ease students in to complex concepts ranging
+                                    from Introductory Chemistry to NMR graph interpretation </p>
+                                </div>
+                                    
                             </div>
                     </div>
                 </div>
