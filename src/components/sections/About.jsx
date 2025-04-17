efa307134a6455a147d902b9ced87ff37cb7c6ee
raven252525 <maximyurkovsky@gmail.com>
2025-04-17 00:37:52 -0700

    edited title of site, and added one project to project section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -22,7 +22,7 @@ export const About = () => {
                         <div className="flex flex-wrap gap-2">
                             {frontendSkills.map((tech, key) => (
                                 <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full
-                                 text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] 
+                                 text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] 
                                  transition">
                                     {tech}
                                 </span>
